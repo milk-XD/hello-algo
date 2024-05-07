f802a3f97bfb832d2eff1246e13de8ebfbabba11
Wentao Hu <wentaohu1017@gmail.com>
2024-05-06 23:38:55 -0700

    5-6
    - LinkedListQueue
    - ArrayQueue

---
+++ codes/java/chapter_stack_and_queue/practice/ArrayQueue.java
@@ -0,0 +1,62 @@
+package chapter_stack_and_queue.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayQueue {
+
+    private final List<Integer> list;
+
+    public ArrayQueue() {
+        list = new ArrayList<>();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(int val) {
+        list.add(val);
+    }
+
+    public int pop() {
+        return list.remove(0);
+    }
+
+    public int peek() {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return list.get(0);
+    }
+
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+}
+
+class ArrayQueueTest {
+
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue();
+        System.out.println("Size: " + queue.size());
+        System.out.println("Empty: " + queue.isEmpty());
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        System.out.println(Arrays.toString(queue.toArray()));
+        System.out.println("Peek: " + queue.peek());
+        queue.pop();
+        queue.pop();
+        System.out.println(Arrays.toString(queue.toArray()));
+    }
+
+}
---
+++ codes/java/chapter_stack_and_queue/practice/LinkedListQueue.java
@@ -0,0 +1,83 @@
+package chapter_stack_and_queue.practice;
+
+import utils.ListNode;
+
+import java.util.Arrays;
+
+public class LinkedListQueue {
+
+    private ListNode head;
+    private ListNode tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(int val) {
+        ListNode cur = new ListNode(val);
+        if (head == null) {
+            head = cur;
+        } else {
+            tail.next = cur;
+        }
+        tail = cur;
+        size++;
+    }
+
+    public int pop() {
+        int val = peek();
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int peek() {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return head.val;
+    }
+
+    public int[] toArray() {
+        ListNode cur = head;
+        int[] res = new int[size()];
+
+        for (int i = 0; i < size(); i++) {
+            res[i] = cur.val;
+            cur = cur.next;
+        }
+
+        return res;
+    }
+
+}
+
+class LinkedListQueueTest {
+
+    public static void main(String[] args) {
+        LinkedListQueue queue = new LinkedListQueue();
+        System.out.println("Size: " + queue.size());
+        System.out.println("Empty: " + queue.isEmpty());
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        System.out.println(Arrays.toString(queue.toArray()));
+        System.out.println("Peek: " + queue.peek());
+        queue.pop();
+        queue.pop();
+        System.out.println(Arrays.toString(queue.toArray()));
+    }
+
+}
