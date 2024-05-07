7d214694a65585237cd7bfe8a1f996bf9d8cd68c
Wentao Hu <wentaohu1017@gmail.com>
2024-05-06 23:04:58 -0700

    5-6
    - LinkedListStack
    - ArrayStack

---
+++ codes/java/chapter_stack_and_queue/practice/ArrayStack.java
@@ -0,0 +1,64 @@
+package chapter_stack_and_queue.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayStack {
+
+    private final List<Integer> stack;
+
+    public ArrayStack() {
+        stack = new ArrayList<>();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(int val) {
+        stack.add(val);
+    }
+
+    public int pop() {
+        int val = peek();
+        stack.remove(size() - 1);
+        return val;
+    }
+
+    public int peek() {
+        if (size() == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return stack.get(size() - 1);
+    }
+
+    public Object[] toArray() {
+        return stack.toArray();
+    }
+
+}
+
+class ArrayStackTest {
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack();
+        System.out.println("Size: " + stack.size());
+        System.out.println("Empty: " + stack.isEmpty());
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(Arrays.toString(stack.toArray()));
+        System.out.println("Peek: " + stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println(Arrays.toString(stack.toArray()));
+    }
+
+}
---
+++ codes/java/chapter_stack_and_queue/practice/LinkedListStack.java
@@ -0,0 +1,74 @@
+package chapter_stack_and_queue.practice;
+
+import utils.ListNode;
+
+import java.util.Arrays;
+
+public class LinkedListStack {
+
+    private ListNode head;
+    private int size;
+
+    public LinkedListStack() {
+        head = null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public int pop() {
+        int val = peek();
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return head.val;
+    }
+
+    public int[] toArray() {
+        ListNode cur = head;
+        int[] res = new int[size()];
+        for (int i = size() - 1; i >= 0; i--) {
+            res[i] = cur.val;
+            cur = cur.next;
+        }
+        return res;
+    }
+
+}
+
+class LinkedListStackTest {
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        System.out.println("Size: " + stack.size());
+        System.out.println("Empty: " + stack.isEmpty());
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(Arrays.toString(stack.toArray()));
+        System.out.println("Peek: " + stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println(Arrays.toString(stack.toArray()));
+    }
+
+}
